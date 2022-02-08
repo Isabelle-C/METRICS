fc207cbe73693947be1a5ec483e29de1ccb27d0d
Isabelle Chen <48035372+Isabelle-C@users.noreply.github.com>
2022-02-08 14:19:28 -0600

    Simulation class skeleton (#12)
    * Added empty biopsy class file
    
    * Update cli skeleton
    
    * Update plot skeleton
    
    * Updating the feature class files
    
    * Updating simulation skeleton
    
    * Delete cli.py
    
    * Delete plot.py
    
    * Delete feature.py
    
    * Delete biopsy.py
    
    * Remove file that doesn't belong to the class
    
    * Remove file that doesn't belong to the class
    
    * Adding docstring in simulation.py
    
    * Reformatting with black
    
    * Update skeleton and re-add cli.py
    
    * Update simulation.py
    
    * Update simulation.py
    
    * Removing decorators and add init

---
+++ src/abmbiopsy/simulation.py
@@ -0,0 +1,68 @@
+class Simulation:
+    """
+    Container for simulated tumor object data.
+    """
+    def __init__(self):
+        '''
+        #TODO: add docstring
+        '''
+        pass
+    def load_simulation_input(input_path):
+        """
+        Load one simulated tumor file into memory.
+
+        Parameters
+        ----------
+        input_path : str
+            File path to a simulation file.
+
+        Returns
+        -------
+        loaded_simulation: dict
+            Loaded simulation file.
+        """
+        pass
+
+    def parse_simulation_input(output_path, loaded_simulation):
+        """
+        Convert raw simulation data into a Dataframe.
+
+        Parameters
+        ----------
+        output_path : str
+            File path for simulation data in matrix form.
+        loaded_simulation : dict
+            Loaded simulation file.
+
+        Returns
+        -------
+        simulation_df : DateFrame
+            Tumor simulation file in a Dataframe.
+        """
+        pass
+        #TODO: save? Based on run time.
+        # grab data from load simulation input
+
+    def make_feature_list(self):
+        """
+        Return the list of valid features
+
+        Returns
+        -------
+        feature_list : list
+           List of valid features that can be extracted.
+        """
+        feature_list = [
+            "APOPT",
+            "QUIES",
+            "MIGRA",
+            "PROLI",
+            "NECRO",
+            "COUNT",
+            "AVG CELL CYCLES",
+            "CELL VOLUMES",
+            "CROWDING TOLERANCE",
+            "METABOLIC PREFERENCE",
+            "MIGRATORY THRESHOLD",
+        ]
+        return feature_list
