13d124b227443f53c81d67815351054bad5c9094
Isabelle Chen <48035372+Isabelle-C@users.noreply.github.com>
2023-01-06 19:08:54 -0600

    Separate punch and needle samples in the workflow (#92)
    * Add the separation of sample needle and sample punch
    
    * Modify workflow to include max radius information from simulation
    
    * Modify the cli to contain appropriate parameters and add docstring
    
    * Add else statement for incorrect sample type
    
    * More changes on workflows
    
    * Fix the parameters and docstring
    
    * Reduce number of parameters
    
    * Resolve mypy errors
    
    Co-authored-by: Isabelle Chen <isabellechen@dhcp-10-105-197-7.wireless.northwestern.private>
    Co-authored-by: Isabelle Chen <isabellechen@Isabelles-MacBook-Pro-2.local>

--- src/abmbiopsy/cli.py
+++ src/abmbiopsy/cli.py
@@ -5,7 +5,7 @@ from abmbiopsy.workflows import run_parse_simulations, run_calculate_stats
 
 
 @click.group()
-def cli():
+def cli() -> None:
     """
     TODO: add docstring
     """
@@ -15,9 +15,16 @@ def cli():
 @click.argument("database")
 @click.argument("simulation")
 @click.option("-t", "--timepoint", default=1.0)
-def parse_simulations(database, simulation, timepoint):
+def parse_simulations(database: str, simulation: str, timepoint: float) -> None:
     """
-    TODO: add docstring
+    Parameters
+    ----------
+    database :
+        File path to database file.
+    simulation :
+        File path to simulation file.
+    timepoint :
+        The timepoint to perform statistical test.
     """
     run_parse_simulations(database, simulation, timepoint)
 
@@ -27,13 +34,28 @@ def parse_simulations(database, simulation, timepoint):
 @click.argument("simulation")
 @click.argument("feature")
 @click.option("-s", "--sample", default="punch")
-@click.option("-r", "--radius", default=1)
 @click.option("-t", "--timepoint", default=1.0)
-def calculate_stats(database, simulation, feature, sample, radius, timepoint):
+@click.option("-r", "--radius", default=1)
+def calculate_stats(
+    database: str, simulation: str, feature: str, sample: str, timepoint: float, radius: int
+) -> None:
     """
-    TODO: add docstring
+    Parameters
+    ----------
+    database :
+        File path to database file.
+    simulation :
+        File path to simulation file.
+    feature :
+        The name of the feature for statistical test.
+    sample : {"Needle", "needle", "punch", "Punch"}
+        The type of the sample.
+    radius :
+        The radius of the punch sample and the width of the needle sample.
+    timepoint :
+        The timepoint to perform statistical test.
     """
-    run_calculate_stats(database, simulation, feature, sample, radius, timepoint)
+    run_calculate_stats(database, simulation, feature, sample, timepoint, radius)
 
 
 if __name__ == "__main__":
--- src/abmbiopsy/workflows.py
+++ src/abmbiopsy/workflows.py
@@ -1,15 +1,23 @@
+from typing import Union
+
 from abmbiopsy.database import Database
 from abmbiopsy.simulation import Simulation
-from abmbiopsy.sample import Sample
+from abmbiopsy.sample_needle import SampleNeedle
+from abmbiopsy.sample_punch import SamplePunch
 from abmbiopsy.stats import Stats
 
 SIMULATION_TABLE = "simulation"
 STATS_TABLE = "stats"
 
 
-def run_parse_simulations(database_file, simulation_file, timepoint):
+def run_parse_simulations(database_file: str, simulation_file: str, timepoint: float) -> None:
     """
-    TODO: add docstring
+    database_file :
+        File path to the database file.
+    simulation_file :
+        File path to the simulation file.
+    timepoint :
+        The timepoint to parse the simulation data.
     """
     database = Database(database_file)
     simulation = Simulation(simulation_file)
@@ -21,17 +29,45 @@ def run_parse_simulations(database_file, simulation_file, timepoint):
 
 
 def run_calculate_stats(
-    database_file, simulation_key, feature_name, sample_shape, sample_radius, timepoint
-):
+    database_file: str,
+    simulation_file: str,
+    feature_name: str,
+    sample_shape: str,
+    timepoint: float,
+    sample_radius: int,
+) -> None:
     """
-    TODO: add docstring
+    Parameters
+    ----------
+    database_file :
+        File path to the database file.
+    simulation_file :
+        File path to the simulation file.
+    feature_name :
+        The name of the feature.
+    sample_shape : {"needle", "Needle", "punch", "Punch"}
+        The shape of the sample.
+    timepoint :
+        The timepoint to perform statistical test.
+    sample_radius :
+        The radius of the punch samples or the width of the needle samples.
     """
     database = Database(database_file)
-    sample = Sample(sample_shape, sample_radius)
-    stats = Stats(simulation_key, sample, timepoint)
+    simulation = Simulation(simulation_file)
+    feature = Simulation.get_feature_object(feature_name)
+
+    sample: Union[SampleNeedle, SamplePunch]
+    if sample_shape in ("needle", "Needle"):
+        sample = SampleNeedle(simulation.max_radius, sample_radius)
+    elif sample_shape in ("punch", "Punch"):
+        sample = SamplePunch(sample_radius)
+    else:
+        raise AttributeError("The sample type provided is not valid.")
+
+    stats = Stats(simulation.key, sample, timepoint, feature)
     print(database)
     print(stats)
 
-    data = database.load_dataframe(SIMULATION_TABLE, simulation_key)
-    stats_df = stats.calculate_feature(feature_name, data)
+    data = database.load_dataframe(SIMULATION_TABLE, simulation.key)
+    stats_df = stats.calculate_feature(data)
     database.add_dataframe(STATS_TABLE, stats_df)
